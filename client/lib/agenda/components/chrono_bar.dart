65488da8df372fa190bc519864ec26d3360df544
Janez Emeršič <noctisbeta@gmail.com>
2025-01-30 14:45:55 +0100

    chrono bar shadow styling

--- client/lib/agenda/components/chrono_bar.dart
+++ client/lib/agenda/components/chrono_bar.dart
@@ -20,14 +20,21 @@ class _ChronoBarState extends State<ChronoBar> with TickerProviderStateMixin {
 
   late final AnimationController _animationController;
   late final AnimationController _shadowAnimationController;
+  late final AnimationController _shadowPulseAnimationController;
 
   late final Animation<double> _animation;
 
   late Animation<double> _shadowAnimation;
+  late Animation<double> _shadowPulseAnimation;
 
   double horizontalDelta = 0;
 
+  double shadowPulseDelta = 0;
+
   double previousHapticAt = 0;
+
+  final double chronoBarLineHeight = 36;
+  final double chronoBarCircleHeight = 72;
 
   @override
   void initState() {
@@ -40,6 +47,11 @@ class _ChronoBarState extends State<ChronoBar> with TickerProviderStateMixin {
     _shadowAnimationController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 300),
+    );
+
+    _shadowPulseAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
     );
 
     _animation = Tween<double>(
@@ -93,6 +105,34 @@ class _ChronoBarState extends State<ChronoBar> with TickerProviderStateMixin {
       ..forward();
   }
 
+  void _shadowPulseAnimationListener() {
+    setState(() {
+      shadowPulseDelta = _shadowPulseAnimation.value;
+    });
+
+    if (_shadowPulseAnimationController.isCompleted) {
+      _shadowPulseAnimationController.reverse();
+    }
+  }
+
+  void _startShadowPulseAnimation() {
+    _shadowPulseAnimationController
+      ..removeListener(_shadowPulseAnimationListener)
+      ..value = 0;
+
+    _shadowPulseAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _shadowPulseAnimationController,
+        curve: Curves.easeInOutSine,
+      ),
+    );
+
+    _shadowPulseAnimationController
+      ..removeListener(_shadowPulseAnimationListener)
+      ..addListener(_shadowPulseAnimationListener)
+      ..forward();
+  }
+
   @override
   Widget build(BuildContext context) => LayoutBuilder(
         builder: (context, constraints) {
@@ -105,16 +145,22 @@ class _ChronoBarState extends State<ChronoBar> with TickerProviderStateMixin {
               final double screenWidth = MediaQuery.of(context).size.width;
 
               return SizedBox(
-                height: 72,
+                height: chronoBarCircleHeight,
                 child: Stack(
                   children: [
                     Positioned(
-                      top: 18 * value,
-                      left: (1 - value) * (widgetMaxWidth - 72) / 2,
-                      right: (1 - value) * (widgetMaxWidth - 72) / 2,
+                      top: chronoBarLineHeight / 2 * value,
+                      left: (1 - value) *
+                          (widgetMaxWidth - chronoBarCircleHeight) /
+                          2,
+                      right: (1 - value) *
+                          (widgetMaxWidth - chronoBarCircleHeight) /
+                          2,
                       child: Container(
-                        width: (screenWidth - 72) * value + 72,
-                        height: -36 * value + 72,
+                        width: (screenWidth - chronoBarCircleHeight) * value +
+                            chronoBarCircleHeight,
+                        height: -chronoBarLineHeight * value +
+                            chronoBarCircleHeight,
                         decoration: BoxDecoration(
                           color: Colors.transparent,
                           borderRadius: BorderRadius.circular(
@@ -123,41 +169,51 @@ class _ChronoBarState extends State<ChronoBar> with TickerProviderStateMixin {
                           boxShadow: [
                             BoxShadow(
                               color: Colors.blue,
-                              spreadRadius: -10,
+                              spreadRadius: -10 + shadowPulseDelta * 10,
                               blurRadius: (20 + horizontalDelta.abs()).abs(),
-                              offset: Offset(
-                                horizontalDelta.clamp(-20, 20),
-                                0,
-                              ),
                             ),
                             BoxShadow(
-                              color: Colors.pink,
-                              spreadRadius:
-                                  -10.0 + (-1 * (horizontalDelta ~/ 5)),
-                              blurRadius: (20 + horizontalDelta.abs()).abs(),
-                              offset: Offset(
-                                horizontalDelta.clamp(-5, 5),
-                                horizontalDelta.clamp(-1, 1),
-                              ),
+                              color: Colors.pinkAccent,
+                              spreadRadius: -10.0 +
+                                  (-1 * (horizontalDelta ~/ 5)) +
+                                  shadowPulseDelta * 7,
+                              blurRadius: (20 + horizontalDelta.abs()).abs() +
+                                  shadowPulseDelta * 3,
+                              // offset: Offset(
+                              //   horizontalDelta.clamp(-5, 5),
+                              //   horizontalDelta.clamp(-1, 1),
+                              // ),
                             ),
                           ],
                         ),
                       ),
                     ),
                     Positioned(
-                      top: 18 * value,
-                      left: (1 - value) * (widgetMaxWidth - 72) / 2,
-                      right: (1 - value) * (widgetMaxWidth - 72) / 2,
+                      top: chronoBarLineHeight / 2 * value,
+                      left: (1 - value) *
+                          (widgetMaxWidth - chronoBarCircleHeight) /
+                          2,
+                      right: (1 - value) *
+                          (widgetMaxWidth - chronoBarCircleHeight) /
+                          2,
                       child: GestureDetector(
                         behavior: HitTestBehavior.translucent,
                         onDoubleTap: () {
+                          _startShadowPulseAnimation();
+
                           if (chronoBarState == ChronoBarState.line) {
                             context.read<ScrollCubit>().resetOffset();
                             unawaited(HapticFeedback.mediumImpact());
                             return;
                           }
+
+                          if (chronoBarState == ChronoBarState.circle) {
+                            unawaited(HapticFeedback.mediumImpact());
+                          }
                         },
                         onLongPress: () {
+                          _startShadowPulseAnimation();
+
                           setState(() {
                             switch (chronoBarState) {
                               case ChronoBarState.line:
@@ -206,7 +262,9 @@ class _ChronoBarState extends State<ChronoBar> with TickerProviderStateMixin {
                             // width = k + n
                             // 72 - width = -k
                             // k = width - 72
-                            width: (screenWidth - 72) * value + 72,
+                            width:
+                                (screenWidth - chronoBarCircleHeight) * value +
+                                    chronoBarCircleHeight,
                             //f(0) = 72
                             //f(1) = 36
                             //f(x) = kx + n
@@ -215,7 +273,8 @@ class _ChronoBarState extends State<ChronoBar> with TickerProviderStateMixin {
                             // 36 = -k
                             // k = -36
                             // f(x) = -36x + 72
-                            height: -36 * value + 72,
+                            height: -chronoBarLineHeight * value +
+                                chronoBarCircleHeight,
                             decoration: BoxDecoration(
                               border: Border.all(
                                 color: kBlack,
@@ -231,6 +290,15 @@ class _ChronoBarState extends State<ChronoBar> with TickerProviderStateMixin {
                                   color: kWhite,
                                   spreadRadius: -20,
                                   blurRadius: 10,
+                                  offset: Offset(
+                                    horizontalDelta.clamp(-20, 20),
+                                    0,
+                                  ),
+                                ),
+                                BoxShadow(
+                                  color: kWhite.withValues(alpha: 0.2),
+                                  spreadRadius: -20,
+                                  blurRadius: 1,
                                   offset: Offset(
                                     horizontalDelta.clamp(-20, 20),
                                     0,
